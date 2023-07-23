bfb05773fad3d9c2536b775f7e200e0a09207261
Alexandre <alexandre@nesga.fr>
2023-07-23 11:16:27 +0200

    feat(pokemons): adding pokemons names
    - improve array performances

--- functions/pokemons/pokemons.js
+++ functions/pokemons/pokemons.js
@@ -1,4 +1,4 @@
-const Pokedex = require ('../../services/PokedexAPI')
+const Pokedex = require('../../services/PokedexAPI')
 
 
 /**
@@ -15,16 +15,20 @@ module.exports.getGenerationList = async (req, res) => {
     let generationDetails = await Promise.all(generations.results.map(generation => PokedexAPI.getGenerationByName(generation.name)))
 
     // append starters pokemons to generations
-    for (const generation of generationDetails) {
-        const [first, second, third] = generation.pokemon_species
-        delete generation.abilities
-        delete generation.moves
-        delete generation.main_region
-        delete generation.version_groups
-        delete generation.types
-        generation.starter = await PokedexAPI.getResource([first.url, second.url, third.url])
-    }
+    // https://stackoverflow.com/questions/5349425/whats-the-fastest-way-to-loop-through-an-array-in-javascript
+    const max = generationDetails.length;
+    let i = 0;
+    for (; i < max; i++) {
+        const [first, second, third] = generationDetails[i].pokemon_species
 
+        delete generationDetails[i].abilities
+        delete generationDetails[i].moves
+        delete generationDetails[i].main_region
+        delete generationDetails[i].version_groups
+        delete generationDetails[i].types
+        generationDetails[i].starter = [first, second, third]
+
+    }
     return res.status(200).send(generationDetails)
 }
 
@@ -51,15 +55,19 @@ module.exports.getPokemonsByGeneration = async (req, res) => {
 
     const pokemons = await PokedexAPI.getResource(range)
 
-    for (const pokemon of pokemons) {
-        delete pokemon.forms
-        delete pokemon.game_indices
-        delete pokemon.is_default
-        delete pokemon.location_area_encounters
-        delete pokemon.moves
-        delete pokemon.past_types
-        delete pokemon.species
+    // https://stackoverflow.com/questions/5349425/whats-the-fastest-way-to-loop-through-an-array-in-javascript
+    const max = pokemons.length;
+    let i = 0;
+    for (; i < max; i++) {
+        delete pokemons[i].forms
+        delete pokemons[i].game_indices
+        delete pokemons[i].is_default
+        delete pokemons[i].location_area_encounters
+        delete pokemons[i].moves
+        delete pokemons[i].past_types
     }
+
+
     // pagination
     const previous = page - 1 > 0 ? page - 1 : null
     const next = ((page + 1) * offset) >= pokemonGenerations.pokemon_species.length ? null : page + 1
@@ -69,7 +77,77 @@ module.exports.getPokemonsByGeneration = async (req, res) => {
 
 module.exports.getPokemonTypes = async (req, res) => {
     const PokedexAPI = await Pokedex
-    const types = await PokedexAPI.getTypesList()
-    console.log(types)
-    res.status(200).send(types)
+    const typeList = await PokedexAPI.getTypesList()
+    let types = await PokedexAPI.getResource(typeList.results.map(type => type.url))
+
+    // https://stackoverflow.com/questions/5349425/whats-the-fastest-way-to-loop-through-an-array-in-javascript
+    const max = types.length;
+    let i = 0;
+    for (; i < max; i++) {
+        delete types[i].generation
+        delete types[i].game_indices
+        delete types[i].pokemons
+        delete types[i].past_damage_relations
+
+        types[i].names = types[i].names.reduce((types, type) => (
+            [
+                ...types,
+                {
+                    [type.language.name]: type.name
+                }
+            ]
+        ), [])
+    }
+
+    return res.status(200).send(types)
+}
+
+module.exports.getPokemonNames = async (req, res) => {
+    const PokedexAPI = await Pokedex
+    //
+    let {generation, page} = req.params
+    const offset = parseInt(process.env.POKEMONS_PER_PAGE)
+
+    page = parseInt(page)
+
+    if (generation === undefined) {
+        return res.status(400).send('No generation provided !')
+    }
+
+    if (page === undefined) {
+        return res.status(400).send('No page provided !')
+    }
+
+    const pokemonGenerations = await PokedexAPI.getGenerationByName(generation)
+    const range = pokemonGenerations.pokemon_species.slice(page * offset, (page + 1) * offset).map(pokemon => {
+        return pokemon.url
+    })
+    let pokemonSpecies = await PokedexAPI.getResource(range)
+
+    console.log(pokemonSpecies)
+    // simplify result
+    pokemonSpecies = pokemonSpecies.reduce((species, specie) => (
+        [
+            ...species,
+            {
+                [specie.name]: {
+                    genus: specie.genera.reduce((genera, genus) => {
+                        return {...genera, [genus.language.name]: genus.genus}
+                    }, {}),
+                    names: specie.names.reduce((names, name) => {
+                        return {...names, [name.language.name]: name.name}
+                    }, {})
+                }
+            }
+        ]
+    ), [])
+    // pagination
+    const previous = page - 1 > 0 ? page - 1 : null
+    const next = ((page + 1) * offset) >= pokemonGenerations.pokemon_species.length ? null : page + 1
+
+    return res.status(200).send({
+        previous: previous,
+        next: next,
+        results: pokemonSpecies
+    })
 }
